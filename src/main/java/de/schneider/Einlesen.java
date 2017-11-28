4ce26b4c73ab2a84eea68d17402be9a5a9cc3432
Bastian Schneider <bastian.schneider@hs-furtwangen.de>
2017-11-28 15:51:28 +0100

    Initial commit

---
+++ src/main/java/de/schneider/Einlesen.java
@@ -0,0 +1,13 @@
+package de.schneider;
+import java.util.Scanner;
+
+public class Einlesen {
+
+	public static void main(String[] args) {
+		String s = "";
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		s = input;
+		System.out.println(s.toUpperCase());
+	}
+}
