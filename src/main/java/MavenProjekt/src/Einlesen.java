6424e3249d33fa25a22b9e7151fa4ad55785a821
Bastian Schneider <bastian.schneider@hs-furtwangen.de>
2017-12-05 15:17:52 +0100

    lles richtig mir doxh egal fick tÂ´dich

--- src/main/java/MavenProjekt/src/Einlesen.java
+++ src/main/java/MavenProjekt/src/Einlesen.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-
-public class Einlesen {
-
-	public static void main(String[] args) {
-		String s = "";
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		s = input;
-		System.out.println(s.toUpperCase());
-	}
-}
+/*
+ * import java.util.Scanner;
+ * 
+ * public class Einlesen {
+ * 
+ * public static void main(String[] args) { String s = ""; Scanner sc = new
+ * Scanner(System.in); String input = sc.nextLine(); s = input;
+ * System.out.println(s.toUpperCase()); } }
+ */
